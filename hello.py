c5e6d8260a5447844010ac4275db5568c7df33af
胡铃松 <39046611+SuperMrh@users.noreply.github.com>
2018-06-23 10:22:47 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print('hello world')
+from random import randint
+
+
+def say_hello():
+    num = randint(1, 3)
+    return 'hello' if num == 1 else 'hello' * 2 if num == 2 else 'hello' * 3
+  
+  
+if __name__ == '__main__':
+    print(say_hello())
